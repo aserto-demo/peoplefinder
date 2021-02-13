2039070b72183afa8e9c9dade018311684626792
Omri Gazitt <ogazitt@gmail.com>
2021-02-13 14:26:11 -0800

    updated build-gcp and deploy-gcp builds to correctly update package version and tag the container image

--- scripts/build-gcp.sh
+++ scripts/build-gcp.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-gcloud builds submit --tag gcr.io/$PROJ/$IMAGE
+
+# bump the patch number in the version 
+VERSION=$(npm version patch)
+
+# strip the 'v' from the vM.m.r
+VERSION=$(echo $VERSION | cut -c 2-)
+
+# submit the build to google cloud build and tag the image with the version
+gcloud builds submit --tag gcr.io/$PROJ/$IMAGE:$VERSION
--- scripts/deploy-gcp.sh
+++ scripts/deploy-gcp.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# extract version from package.json
+VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
 gcloud run deploy $SVC \
-  --image gcr.io/$PROJ/$IMAGE \
+  --image gcr.io/$PROJ/$IMAGE:$VERSION \
   --platform managed --allow-unauthenticated
