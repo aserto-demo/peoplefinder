3df267c6d63962b1926ea58f31a2ed9b6ff0f641
Omri Gazitt <ogazitt@gmail.com>
2021-02-13 15:18:04 -0800

    display version on home page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,11 +3,15 @@ import logo from '../assets/logo.svg'
 
 const Hero = () =>
   <div className="text-center hero my-3">
-    <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
+    <img className="mb-3 app-logo" src={logo} alt="Aserto logo" width="120" />
     <h1 className="mb-4">PeopleFinder</h1>
 
     <p className="lead">
       Find and manage users.
+    </p>
+
+    <p>
+      Version: {process.env.REACT_APP_VERSION || 'development'}
     </p>
   </div>
 
